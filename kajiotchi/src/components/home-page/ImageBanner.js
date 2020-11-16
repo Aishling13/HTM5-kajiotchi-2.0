cd7034ef8cb8d0063d6fd7a382f3cb39d70fd4a1
DESKTOP-A3386MS\chloe <chloe.lodge@outlook.com>
2020-11-16 19:49:35 +0000

    added images

--- kajiotchi/src/components/home-page/ImageBanner.js
+++ kajiotchi/src/components/home-page/ImageBanner.js
@@ -1,9 +1,24 @@
 import './Home.css';
+import childcry from 'images/pixelimagechildcry.gif';
+import childblue from 'images/childblue.gif';
+import childorange from 'images/childorange.gif';
+import childpurple from 'images/childpurple.gif';
+import childred from 'images/childred.gif';
 
 function ImageBanner() {
+  const images = [childblue, childcry, childorange, childcry, childpurple, childcry, childred];
   return ( 
-    <div className='ImageBanner'/>
+    <div className='ImageBanner'>
+      {images.map(img => <ImageContainer imageSrc={img}/>)}
+    </div>
   )
 }
 
+function ImageContainer({imageSrc}) {
+  return ( 
+    <div className='ImageContainer'>
+      <img src={imageSrc} className="Image"/>
+    </div>
+  )
+}
 export default ImageBanner;
